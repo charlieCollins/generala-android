51a73c05ba37ed848e66cdb9ae7cae9ba137d2d4
Charlie Collins <charles.collins@disney.com>
2016-06-26 00:27:49 -0400

    Initial commit

---
+++ app/src/main/java/generala/totsp/com/generala/AboutActivity.java
@@ -0,0 +1,15 @@
+package generala.totsp.com.generala;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+
+    }
+}
---
+++ app/src/main/java/generala/totsp/com/generala/MainActivity.java
@@ -0,0 +1,69 @@
+package generala.totsp.com.generala;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageButton dice1Button;
+    private ImageButton dice2Button;
+    private ImageButton dice3Button;
+    private ImageButton dice4Button;
+    private ImageButton dice5Button;
+    private ToggleButton dice6Button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dice1Button = (ImageButton) findViewById(R.id.diceButton1);
+        dice2Button = (ImageButton) findViewById(R.id.diceButton2);
+        dice3Button = (ImageButton) findViewById(R.id.diceButton3);
+        dice4Button = (ImageButton) findViewById(R.id.diceButton4);
+        dice5Button = (ImageButton) findViewById(R.id.diceButton5);
+        dice6Button = (ToggleButton) findViewById(R.id.diceButton6);
+
+        dice6Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "click", Toast.LENGTH_SHORT).show();
+                Log.d("GENERALA", "dice6 selected:" + dice6Button.isSelected());
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
